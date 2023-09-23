666f269842f68fa05f330d1743694c9a8d614950
DrimoZ <drimozbe@gmail.com>
2023-09-23 11:42:12 +0200

    verif masque + frame 2 reset

--- mainApp.py
+++ mainApp.py
@@ -41,6 +41,8 @@ class MainApplication(Tk):
     # to display the current frame passed as parameter
     def show_frame(self, cont):
         frame = self.frames[cont]
+        if (not isinstance(frame, HomePage)):
+            frame.reset()
         frame.tkraise()
   
 # Page d'acceuil
@@ -153,9 +155,9 @@ class Application1(Frame):
 
 
 
-        # Fonction de reset de la frame
-        def reset(self) -> None:
-            pass
+    # Fonction de reset de la frame
+    def reset(self) -> None:
+        pass
         
 
 
@@ -171,98 +173,98 @@ class Application2(Frame):
         Frame.__init__(self, parent)
 
         # Frame - Données d'entrée
-        infoFrame = Frame(self, highlightbackground="black", highlightthickness=1)
-        infoFrame.grid_propagate(0)
-        infoFrame.config(width=270, height=210)
+        self.infoFrame = Frame(self, highlightbackground="black", highlightthickness=1)
+        self.infoFrame.grid_propagate(0)
+        self.infoFrame.config(width=270, height=210)
 
         # Labels - Données d'entrée
-        labIp = Label(infoFrame, text="IP * : ", justify="left").grid(row = 1, column = 0, padx = 10, pady = 10)
-        labMasque = Label(infoFrame, text="Masque : ", justify="left").grid(row = 2, column = 0, padx = 10, pady = 10)
-        labReseau = Label(infoFrame, text="Réseau * : ", justify="left").grid(row = 3, column = 0, padx = 10, pady = 10)
+        labIp = Label(self.infoFrame, text="IP * : ", justify="left").grid(row = 1, column = 0, padx = 10, pady = 10)
+        labMasque = Label(self.infoFrame, text="Masque : ", justify="left").grid(row = 2, column = 0, padx = 10, pady = 10)
+        labReseau = Label(self.infoFrame, text="Réseau * : ", justify="left").grid(row = 3, column = 0, padx = 10, pady = 10)
 
         # Entries - Données d'entrée
-        textIp = Entry(infoFrame, width=25)
-        textIp.grid(row = 1, column = 1, padx = 10, pady = 10)
-        textIp.insert(0, "0.0.0.0")
-        textMasque = Entry(infoFrame, width=25)
-        textMasque.grid(row = 2, column = 1, padx = 10, pady = 10)
-        textMasque.insert(0, "0.0.0.0")
-        textReseau = Entry(infoFrame, width=25)
-        textReseau.grid(row = 3, column = 1, padx = 10, pady = 10)
-        textReseau.insert(0, "0.0.0.0")
+        self.textIp = Entry(self.infoFrame, width=25)
+        self.textIp.grid(row = 1, column = 1, padx = 10, pady = 10)
+        self.textIp.insert(0, "0.0.0.0")
+        self.textMasque = Entry(self.infoFrame, width=25)
+        self.textMasque.grid(row = 2, column = 1, padx = 10, pady = 10)
+        self.textMasque.insert(0, "0.0.0.0")
+        self.textReseau = Entry(self.infoFrame, width=25)
+        self.textReseau.grid(row = 3, column = 1, padx = 10, pady = 10)
+        self.textReseau.insert(0, "0.0.0.0")
 
         # Errors - Données d'entrée
-        attStr = StringVar()
-        attStr.set("")
-        lblVerif = Label(infoFrame, textvariable=attStr, justify="center", fg="red")
+        self.attStr = StringVar()
+        self.attStr.set("")
+        lblVerif = Label(self.infoFrame, textvariable=self.attStr, justify="center", fg="red")
         lblVerif.grid(row = 4, column = 0, columnspan=2, padx = 10, pady = 10)
 
         # Buttons - Données d'entrée
-        btnCheck = Button(infoFrame, text="Verifier l'adresse Ip", cursor="hand2")
-        btnCheck.config(command= lambda: checkReseau())
+        btnCheck = Button(self.infoFrame, text="Verifier l'adresse Ip", cursor="hand2")
+        btnCheck.config(command= lambda: self.checkReseau())
         btnCheck.grid(row = 5, column = 1, padx = 10, pady = 10)
 
         # Fin de la Frame d'entrée
-        infoFrame.place(x=30, y=30)
+        self.infoFrame.place(x=30, y=30)
 
 
         # Frame - Titre
-        titleFrame = Frame(self, highlightbackground="black", highlightthickness=1, width=SIZE_X-360, height=90)
-        titleFrame.grid_propagate(0)
-        titleFrame.place(x=330, y=30)
+        self.titleFrame = Frame(self, highlightbackground="black", highlightthickness=1, width=SIZE_X-360, height=90)
+        self.titleFrame.grid_propagate(0)
+        self.titleFrame.place(x=330, y=30)
 
         # Labels - Titre
-        lblTitre = Label(titleFrame, text="Application 2 : Présence d'une IP dans le réseau", font = 'Times 14 underline' )
+        lblTitre = Label(self.titleFrame, text="Application 2 : Présence d'une IP dans le réseau", font = 'Times 14 underline' )
         lblTitre.place(x=10, y=10)
-        lblExo = Label(titleFrame, fg='blue', text="Sur base d’une IP, d’une adresse de réseau (et d'un masque si découpé en sous-réseau),\nvérifie si l’adresse IP donnée appartient au réseau ou pas. ", font = 'Times 11 italic', justify="left" )
+        lblExo = Label(self.titleFrame, fg='blue', text="Sur base d’une IP, d’une adresse de réseau (et d'un masque si découpé en sous-réseau),\nvérifie si l’adresse IP donnée appartient au réseau ou pas. ", font = 'Times 11 italic', justify="left" )
         lblExo.place(x=10, y=40)
 
 
         # Frame - Réponses
-        repFrame = Frame(self, highlightbackground="black", highlightthickness=1, width=SIZE_X-360, height=300)
-        repFrame.grid_propagate(0)
+        self.repFrame = Frame(self, highlightbackground="black", highlightthickness=1, width=SIZE_X-360, height=300)
+        self.repFrame.grid_propagate(0)
 
         # Labels - Titre
-        rep = StringVar()
-        rep.set("OUI")
-        lblVerif = Label(repFrame, textvariable=rep, justify="center", fg="red")
+        self.rep = StringVar()
+        self.rep.set("OUI")
+        lblVerif = Label(self.repFrame, textvariable=self.rep, justify="center", fg="red")
         lblVerif.place(x=0, y=0)
 
 
-        def checkReseau() -> None :
-            """
-            Récupere Ip, Masque et Réseau entrés. Vérifie que tout soit valide. Explique si l'Ip fait partie du Réseau donné. 
-            """
-            attStr.set("")
-            repFrame.place_forget()
+    def checkReseau(self) -> None :
+        """
+        Récupere Ip, Masque et Réseau entrés. Vérifie que tout soit valide. Explique si l'Ip fait partie du Réseau donné. 
+        """
 
-            # Un des champs est vide
-            if (textIp.get() == ""):
-                attStr.set("(*) Veuillez entrer une adresse IP")
-                return
-            elif (textReseau.get() == ""):
-                attStr.set("(*) Veuillez entrer une adresse de réseau")
-                return
+        self.attStr.set("")
+        self.repFrame.place_forget()
 
-            # Instance de Reseau
-            res = Reseau(textIp.get(), textMasque.get(), textReseau.get())
-
-            # Vérification des champs
-            if (res.ip == "0.0.0.0"):
-                attStr.set("Adresse IP non-valide")
-            elif (res.masque == "0.0.0.0"):
-                attStr.set("Masque Réseau non-valide")
-            elif (res.adrReseau == "0.0.0.0"):
-                attStr.set("Adresse Réseau non-valide")
-            else:
-                repFrame.place(x=330, y=150)
+        # Un des champs est vide
+        if (self.textIp.get() == "" or self.textReseau.get() == ""):
+            self.attStr.set("(*) Champs requis : " + ("IP" if self.textIp.get() == "" else "") + (" et " if self.textIp.get() == "" and self.textReseau.get() == "" else "") + ("Adresse de réseau" if self.textReseau.get() == "" else ""))
+            return
         
-        # Fonction de reset de la frame
-        def reset(self) -> None:
-            attStr.set("")
-            textIp.delete(0, END)
-            textMasque.delete(0, END)
-            textReseau.delete(0, END)
+        # Instance de Reseau
+        res = Reseau(self.textIp.get(), self.textMasque.get(), self.textReseau.get())
+
+        # Vérification des champs
+        if (res.ip == "0.0.0.0"):
+            self.attStr.set("Adresse IP non-valide")
+        elif (res.masque == "0.0.0.0" and self.textMasque.get() != ""):
+            self.attStr.set("Masque Réseau non-valide")
+        elif (res.adrReseau == "0.0.0.0"):
+            self.attStr.set("Adresse Réseau non-valide")
+        else:
+            self.repFrame.place(x=330, y=150)
+        
+
+    # Fonction de reset de la frame
+    def reset(self) -> None:
+        self.attStr.set("")
+        self.textIp.delete(0, END)
+        self.textMasque.delete(0, END)
+        self.textReseau.delete(0, END)
+        self.repFrame.place_forget()
 
 
 class Application3(Frame):
@@ -291,9 +293,9 @@ class Application3(Frame):
 
 
 
-        # Fonction de reset de la frame
-        def reset(self) -> None:
-            pass
+    # Fonction de reset de la frame
+    def reset(self) -> None:
+        pass
 
 
 class Reseau():
@@ -306,78 +308,64 @@ class Reseau():
     - Adresse du Sous Réseau
     """
 
-    def __init__(self, ip, masque, adrReseau="0.0.0.0") -> None:
+    def __init__(self, ip: str, masque: str, adrReseau: str = "0.0.0.0") -> None:
         if (Reseau.ipValide(ip)):
-            self.ip = ip
+            self.ip: str = ip
         else:
-            self.ip = "0.0.0.0"
+            self.ip: str = "0.0.0.0"
         if (Reseau.masqueValide(masque)):
-            self.masque = masque
+            self.masque: str = masque
         else:
-            self.masque = "0.0.0.0"
+            self.masque: str = "0.0.0.0"
         if (Reseau.reseauValide(adrReseau)):
-            self.adrReseau = adrReseau
+            self.adrReseau: str = adrReseau
         else:
-            self.adrReseau = "0.0.0.0"
+            self.adrReseau: str = "0.0.0.0"
         
-        self.adrBroadCast = "0.0.0.0"
-        self.adrSR = "0.0.0.0"
+        self.adrBroadCast: str = "0.0.0.0"
+        self.adrSR: str = "0.0.0.0"
         
-    def ipValide(ip) -> bool:
+    def ipValide(ip: str) -> bool:
         return True
 
-    def masqueValide(masque) -> bool:
+    def masqueValide(masque: str) -> bool:
+        masque = masque.strip().lower()
+        octets = masque.split('.')
+
+        # Check if there are exactly 4 octets
+        if len(octets) != 4:
+            return False
+        
+        # Initialize a flag to track contiguous 1s
+        contiguous_ones = True
+
+        for octet in octets:
+            try:
+                # Convert the octet to an integer
+                octet_value = int(octet)
+                # Check if the octet is within the valid range [0, 255]
+                if octet_value < 0 or octet_value > 255:
+                    return False
+                # Check if the octet is 255 (contiguous 1s)
+                if contiguous_ones:
+                    if octet_value != 255:
+                        contiguous_ones = False
+                else:
+                    # Check if the octet is 0 (contiguous 0s)
+                    if octet_value != 0:
+                        return False
+                    
+            except ValueError:
+                # If an octet is not a valid integer, return False
+                return False
+            
+        # Check if there is at least one octet with contiguous 0s
+        if contiguous_ones:
+            return False
+        
         return True
-#         octets = subnet_mask.split('.')
 
-#     # Check if there are exactly 4 octets
-#     if len(octets) != 4:
-#         return False
-
-#     # Initialize a flag to track contiguous 1s
-#     contiguous_ones = True
-
-#     for octet in octets:
-#         try:
-#             # Convert the octet to an integer
-#             octet_value = int(octet)
-
-#             # Check if the octet is within the valid range [0, 255]
-#             if octet_value < 0 or octet_value > 255:
-#                 return False
-
-#             # Check if the octet is 255 (contiguous 1s)
-#             if contiguous_ones:
-#                 if octet_value != 255:
-#                     contiguous_ones = False
-#             else:
-#                 # Check if the octet is 0 (contiguous 0s)
-#                 if octet_value != 0:
-#                     return False
-#         except ValueError:
-#             # If an octet is not a valid integer, return False
-#             return False
-
-#     # Check if there is at least one octet with contiguous 0s
-#     if contiguous_ones:
-#         return False
-
-#     return True
-
-# # Get the subnet mask from the user
-# subnet_mask = input("Enter a subnet mask in the format xxx.xxx.xxx.xxx: ")
-
-# # Remove leading and trailing spaces and convert to lowercase for case-insensitivity
-# subnet_mask = subnet_mask.strip().lower()
-
-# # Check if it's a valid subnet mask
-# if is_valid_subnet_mask(subnet_mask):
-#     print("Valid subnet mask")
-# else:
-#     print("Invalid subnet mask")
-
-
-    def reseauValide(adrReseau) -> bool:
+    def reseauValide(adrReseau: str) -> bool:
         return True
     
   
