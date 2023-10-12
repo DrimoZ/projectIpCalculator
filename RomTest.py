56b789d37e267e23c84848d34a5956140b098dae
polomo080 <polomo080@gmail.com>
2023-10-12 23:39:07 +0200

    s

--- RomTest.py
+++ RomTest.py
@@ -1,17 +1,29 @@
-import tkinter as tk
+import ipaddress
 
+# Saisie de l'adresse IP et du masque de sous-réseau au format CIDR (par exemple, 192.168.1.0/24)
+ip_cidr = input("Entrez l'adresse IP et le masque CIDR (ex: 192.168.1.0/24) : ")
 
-def rand_func(eff=None, a=1, b=2, c=3):
-    print(a + b + c)
+# Convertir l'entrée en objet IPNetwork
+network = ipaddress.ip_network(ip_cidr, strict=False)
+print(network)
 
-root = tk.Tk()
-root.bind("<Return>", lambda eff: rand_func(eff, a=10, b=20, c=30))
+# Demander le nombre de sous-réseaux souhaité
+num_subnets = int(input("Entrez le nombre de sous-réseaux souhaité : "))
 
-frame = tk.Frame(root)
-frame.pack()
+# Calculer le masque de sous-réseau approprié pour le nombre de sous-réseaux
+subnet_mask_length = network.prefixlen + num_subnets.bit_length() - 1
+print(network.prefixlen)
+print(subnet_mask_length)
 
-button = tk.Button(frame, text="click me",
-                   command=lambda: rand_func(None, 1, 2, 3))
-button.pack()
-
-root.mainloop()
+if subnet_mask_length > 32:
+    print("Nombre de sous-réseaux souhaité trop élevé pour l'adresse IP donnée.")
+else:
+    subnet = network.subnets(new_prefix=subnet_mask_length)
+    subnets_list = list(subnet)
+    
+    # Afficher les informations sur les sous-réseaux créés
+    print(f"Adresse IP d'origine : {network.network_address}/{network.prefixlen}")
+    print(f"Masque de sous-réseau pour {num_subnets} sous-réseaux : /{subnet_mask_length}")
+    
+    for i, sub in enumerate(subnets_list):
+        print(f"Sous-réseau {i+1} : {sub.network_address}/{subnet_mask_length}")
